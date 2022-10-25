d63046d7b99b0fe51e1aaca48bc700298841798f
Ismael Contreras <70118215+skeletongp@users.noreply.github.com>
2022-10-25 14:16:29 -0400

    To test edit

--- src/Views/table.blade.php
+++ src/Views/table.blade.php
@@ -67,7 +67,6 @@
                                                 $edit->setTable($element->table);
                                                 $edit->setKeyName($element->key);
                                                 $edit->setKeyValue($item->{$element->key});
-                                                
                                             @endphp
                                         @break
                                     @endswitch
