40d65f6489dff1334524f483f5d79d6e1deea441
Ismael Contreras <70118215+skeletongp@users.noreply.github.com>
2022-10-25 11:28:20 -0400

    updated autoload

--- src/Providers/SimpletableServiceProvider.php
+++ src/Providers/SimpletableServiceProvider.php
@@ -8,7 +8,6 @@ class SimpletableServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-      //add /src/Commands/NewDatatable command to project's commands
         if ($this->app->runningInConsole()) {
             $this->commands([
                 NewDatatable::class,
